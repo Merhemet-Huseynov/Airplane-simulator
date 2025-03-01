7a0c954af81e9e513df0b628d22714367f04605d
https://github.com/Merhemet-Huseynov <merhemethuseynov795@gmail.com>
2025-03-01 15:55:44 +0400

    Refactor SpeedOperations to use random max speed and modify output flow
    - Added max_speed() function to generate random target speed for the plane.
    - Modified increase_speed to increase speed until a randomly generated max speed is reached, with the plane taking off when reaching 200 km/h.
    - Changed speed output to print intermediate steps and indicate when the target speed is reached.
    - Incorporated decrease_speed to start decreasing speed once the target speed is reached.
    - Updated decrease_speed to print the current speed and indicate when the plane lands.

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,2 @@
 from .passenger import random_passenger_count, generate_fake_passengers
+from .speed import max_speed
---
+++ data/speed.py
@@ -0,0 +1,7 @@
+import random
+
+def max_speed() -> int:
+    """
+    This function generates a random speed between 600 and 900.
+    """
+    return random.randint(600, 900)
